b9c04fff179d0941281ad679e4461947556b5d32
PranathiB <pranathi.birudugadda@goeuro.com>
2017-08-02 21:27:42 +0200

    Simple assert that there is at least one movie result on the page

--- src/test/TopMovieListTest.java
+++ src/test/TopMovieListTest.java
@@ -1,10 +1,15 @@
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class TopMovieListTest {
     private static ChromeDriver driver;
@@ -24,5 +29,7 @@ public class TopMovieListTest {
     @Test
     public void shouldHaveAtLeastOneMovieOnTopShowsPage(){
         driver.get("http://www.imdb.com/chart/top");
+        List<WebElement> moviesList = driver.findElements(By.cssSelector(".lister-list tr"));
+        Assert.assertTrue("At least one movie is expected in the list",moviesList.size() >= 1);
     }
 }
